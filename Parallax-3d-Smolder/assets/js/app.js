3f07781d5eb5f03ae2985d92217438c4e04fc568
GiveMeYourMora <125956689+GiveMeYouMora@users.noreply.github.com>
2024-03-26 19:50:40 +1000

    Delete Parallax-3d-Smolder directory

--- Parallax-3d-Smolder/assets/js/app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("mousemove", (e) => {
-    Object.assign(document.documentElement, {
-        style: `
-        --moveX: ${(e.clientX - window.innerWidth / 2) * -0.005}deg;
-        --moveY: ${(e.clientY - window.innerHeight / 2) * -0.01}deg;
-        `,
-    });
-});
